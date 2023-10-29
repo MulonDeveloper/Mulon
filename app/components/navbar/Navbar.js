f0a56274dce46b4b116e0439d2cb76e2e2a5b02d
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-10-30 02:57:46 +0700

    navigation update

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,26 +1,30 @@
 "use client"
-import React from 'react'
-import styles from './style.module.scss'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import styles from './style.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useMediaQuery } from 'react-responsive';
+import { IoIosArrowDown } from 'react-icons/io';
+import { HiMenu, HiX } from 'react-icons/hi';
+import { motion, useAnimation } from "framer-motion"
 import logoPutih from '../../../public/mulonPutih.svg';
 import logoHitam from '../../../public/mulonSamping.png';
-import logoResponsive from '../../../public/Mulon.png';
-import hamburgerIcon from '../../../public/asset/icons/hamburger.png'
-import { motion, useAnimation } from "framer-motion"
 
-import { HiMenu } from "react-icons/hi";
-export const Navbar = () => {
+const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
     const [prevScrollY, setPrevScrollY] = useState(0);
     const controls = useAnimation();
-    const initialPosition = scrolled ? -22 : +20;
-    
+    const isMobileView = useMediaQuery({ maxWidth: 768 });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const initialPosition = scrolled ? -22 : 20;
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     useEffect(() => {
         const handleScroll = () => {
             const currentScrollY = window.scrollY;
-
 
             if (currentScrollY > 5 && !scrolled) {
                 setScrolled(true);
@@ -28,97 +32,97 @@ export const Navbar = () => {
                 setScrolled(false);
             }
 
-            // Check if scrolling up and if the navbar is not fully visible
             if (currentScrollY < prevScrollY && !scrolled) {
-                controls.start({ y: 0 });
+                if (!isMobileView) {
+                    controls.start({ y: 0 });
+                }
             } else {
-                controls.start({ y: -60 }); // Adjust this value to control how much the navbar slides down
+                controls.start({ y: -60 });
             }
 
-            const isMobileView = window.matchMedia('(max-width: 768px)').matches;
-
             if (isMobileView) {
-                // You can set specific styles for mobile view here
                 controls.start({ y: 0 });
             } else {
-                controls.start({ y: initialPosition });
+                controls.start({ y: scrolled ? -22 : 20 });
             }
 
             setPrevScrollY(currentScrollY);
         };
+
         window.addEventListener('scroll', handleScroll);
 
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, [scrolled, prevScrollY, controls]);
+    }, [scrolled, prevScrollY, controls, isMobileView]);
 
     const logoSrc = scrolled ? logoPutih : logoHitam;
     const navbar = scrolled ? `${styles.navbar} ${styles.scrolled}` : styles.navbar;
 
     const navbarAnimation = {
-        backgroundColor: scrolled ? '#549b79' : 'transparent',
-        boxShadow: scrolled ? '0px 0px 10px rgba(0, 0, 0, 0.1)' : 'none',
+        backgroundColor: scrolled || isMobileView ? '#549b79' : 'transparent',
+        boxShadow: scrolled || isMobileView ? '0px 0px 10px rgba(0, 0, 0, 0.1)' : 'none',
         transition: 'background-color 0.3s ease, box-shadow 0.3s ease',
     };
 
     return (
-        <>
-            <nav className={styles.container}>
-
-                <motion.div
-                    animate={{ ...navbarAnimation, y: initialPosition }}
-                    initial={false}
-                    style={{ y: controls }}
-                    className={navbar}>
-                    <div className={styles.wrapLogo}>
-                        <div className={styles.navLogo}>
-                            <Link href={'/'}>
-                                <Image
-                                    src={logoSrc}
-                                    alt='Mulon Logo'
-                                    width={100}
-                                    height={33}
-                                />
+        <nav className={styles.container}>
+            <motion.div
+                animate={{ ...navbarAnimation, y: isMobileView ? 0 : (scrolled ? -22 : 20) }}
+                initial={false}
+                style={{ y: controls }}
+                className={navbar}
+            >
+                <div className={styles.wrapLogo}>
+                    <div className={styles.navLogo}>
+                        <Link href={'/'}>
+                            <Image src={logoSrc} alt="Mulon Logo" width={100} height={33} />
+                        </Link>
+                    </div>
+                    <>
+                        <ul className={`${styles.navbar__items} ${isMenuOpen ? (`${styles.active}`) : (`${styles.none}`)}`}>
+                            <Link href="/">
+                                <li>Home</li>
                             </Link>
-
-                        </div>
-                        <ul className={styles.navbar__items}>
-                            <li>
-                                <Link href="/">
-                                    Home
-                                </Link>
+                            <li className={styles.nestedLi}>
+                                <p className={styles.options}>
+                                    Services <IoIosArrowDown />
+                                </p>
+                                <ul className={styles.nestedUL}>
+                                    <Link href="/">
+                                        <li>Angkut sampah</li>
+                                    </Link>
+                                    <Link href="/">
+                                        <li>Pengembangan IoT</li>
+                                    </Link>
+                                    <Link href="/">
+                                        <li>Pemberdayaan Masyarakat</li>
+                                    </Link>
+                                </ul>
                             </li>
                             <li>
-                                <Link href="#">
-                                    Angkut Sampah
-                                </Link>
+                                <Link href="#">Activity</Link>
                             </li>
                             <li>
-                                <Link href="#">
-                                    Activity
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="#">
-                                    Blog
-                                </Link>
-
+                                <Link href="#">Blog</Link>
                             </li>
                         </ul>
-                        {/* <div className={styles.hamburgerPlace} >
-                            <HiMenu width={40} height={40} />
-                        </div> */}
+                    </>
+
+
+                    <div className={styles.hamburgerPlace} onClick={toggleMenu}>
+                        {isMenuOpen ? (
+                            <HiX width={50} height={50} />
+                        ) : (
+                            <HiMenu width={50} height={50} />
+                        )}
                     </div>
 
+                </div>
 
-                </motion.div>
+            </motion.div>
+        </nav>
+    );
+};
 
-            </nav >
-        </>
-
-    )
-}
-
-
-export default Navbar
+export default Navbar;
